a056073dff8fd906aa0725dfd3f6799ca2c035db
Ijeoma <jilado10@gmail.com>
2025-01-08 06:40:05 +0100

    Updated both Frontend and Admin build

--- Frontend/src/Utils/ApiRoutes.js
+++ Frontend/src/Utils/ApiRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:4000"
+// export const host = "http://localhost:4000"
+export const host = "https://uc-event-management-backend-f1f1833e6850.herokuapp.com"
 export const loginRoute = `${host}/login`
 export const signUpRoute = `${host}/signup`
 export const displaySelectedPortfolioRoute = `${host}/portfolio`
