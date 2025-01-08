1dbd6f7163cfcf7a25a1a5a60f02a125f1bfa1ae
Ijeoma <jilado10@gmail.com>
2025-01-08 06:23:12 +0100

    Update AdminApiRoutes.js

--- Admin/src/Utils/AdminApiRoutes.js
+++ Admin/src/Utils/AdminApiRoutes.js
@@ -1,6 +1,6 @@
 // localhos is used locall while the other one is hosted on heroku
 // export const host = "http://localhost:4000"
-export const host = "https://git.heroku.com/uc-event-management-backend.git"
+export const host = "https://uc-event-management-backend-f1f1833e6850.herokuapp.com"
 export const uploadImageRoute = `${host}/upload`
 export const addPortfolioRoute = `${host}/addportfolio`
 export const removePortfolioRoute = `${host}/removeportfolio`
